a856625d5ba0d8ebfad792c4ddc91c8b65b7e7ee
nana_rukmana <nanark92@gmail.com>
2016-11-14 13:53:48 +0700

    task two

---
+++ app/src/main/java/layout/ProfilFragment.java
@@ -0,0 +1,30 @@
+package layout;
+
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.nanark.tasktwo.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ProfilFragment extends Fragment {
+
+
+    public ProfilFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+        return inflater.inflate(R.layout.fragment_profil,container,false);
+    }
+
+
+}
